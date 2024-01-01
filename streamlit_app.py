3afd80ac7a316540b0f4380b57a06c18c4a6592c
Erikson <erikson99.id@gmail.com>
2024-01-01 21:24:10 +0700

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('My DABW Snowflake Streamlit App')
 
+streamlit.header('Breakfaast Menu')
+streamlit.test('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
